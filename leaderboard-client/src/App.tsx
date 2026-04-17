701a7fbc2c3c83f699e2907a6c4e086bc6183ab2
conthom <conthom@iu.edu>
2026-04-17 19:59:26 -0400

    build fixes

---
+++ leaderboard-client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { HistoryPage } from "./pages/HistoryPage";
+import { HomePage } from "./pages/HomePage";
+import { StatsPage } from "./pages/StatsPage";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/log" element={<HistoryPage />} />
+      </Route>
+    </Routes>
+  );
+}
